a17aab76976bb8bd345ee2876611577be225fda4
zhanta <zhantoreyesen@gmail.com>
2023-03-09 21:12:51 +0600

    several changes

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ const app = express()
 
 // Connection to MySQL database
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 })
 
 connection.connect(function (err) {
@@ -44,6 +45,54 @@ app.use(express.static("public"))
 app.get("/", (req, res) => {
   res.render("index.ejs")
 })
+
+app.get("/auth", (req,res) =>{
+  res.render("auth.ejs")
+})
+app.get("/reg", (req,res) =>{
+  res.render("reg.ejs")
+})
+
+
+app.post('/auth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		connection.query('SELECT * FROM users WHERE IIN = ? AND Password = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = username;
+				// Redirect to home page
+				response.redirect('/about');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+app.get('/about', function(request, response) {
+	// If the user is loggedin
+	if (request.session.loggedin) {
+		// Output username
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		// Not logged in
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
 
 // Start the server and listen for incoming requests on a specified port
 app.listen(process.env.PORT, (err) => {
